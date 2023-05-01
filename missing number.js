49ed069dd3cb39c27d675f4397128a0f1e1bc158
Vishnurajmr1 <vishnurajmr1@gmail.com>
2023-05-01 10:52:29 +0530

    fourth Program

---
+++ missing number.js
@@ -0,0 +1,15 @@
+// nums=[3,0,1];
+//output:2;
+//nums=[0,1];
+//output:2;
+
+var missingNumber=function(nums){
+    let n=nums.length;
+    let expectedSum=(n*(n+1)/2);
+    let actualSum=0;
+    for(let i=0;i<n;i++){
+        actualSum +=nums[i];
+    }
+    return expectedSum-actualSum;
+}
+console.log(missingNumber([3,0,1]));
